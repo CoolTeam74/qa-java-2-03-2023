5f0c37444269d518680600616f52bb76a500675a
A.Polyakov <a.polyakov@unlimint.com>
2023-03-14 10:07:31 +0300

    add robot service example

--- unit-tests/src/test/java/GameTest.java
+++ unit-tests/src/test/java/GameTest.java
@@ -5,6 +5,7 @@ import org.example.GameWinnerPrinter;
 import org.example.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -13,7 +14,7 @@ public class GameTest {
     @Test
     public void testPlayerOneWIn() {
         Dice dice = Mockito.mock(Dice.class);
-        Mockito.when(dice.roll()).thenReturn(6, 5);
+        BDDMockito.given(dice.roll()).willReturn(6, 5);
         GameWinnerPrinter gameWinnerPrinter = Mockito.mock(GameWinnerPrinter.class);
         Mockito.doAnswer(answer -> {
             Player actualWinner = answer.getArgument(0);
